7dff1643e8d262e9932ac502d8eddff69fed85bb
Developer Yusuf <developeryusuf@icloud.com>
2022-02-09 19:48:27 +0300

    Register eklendi

--- lib/controllers/RegisterController.dart
+++ lib/controllers/RegisterController.dart
@@ -1,3 +1,25 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get_state_manager/get_state_manager.dart';
+import 'package:get/get.dart';
 
-class RegisterController extends GetxController {}
+class RegisterController extends GetxController {
+  Register(username, password) async {
+    print("mail: " + username + " " + "şifre: " + password);
+
+    try {
+      UserCredential userCredential =
+          await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: username.toString(),
+        password: password.toString(),
+      );
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        print('The password provided is too weak.');
+      } else if (e.code == 'email-already-in-use') {
+        print('The account already exists for that email.');
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+}
--- lib/screens/RegisterScreen.dart
+++ lib/screens/RegisterScreen.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:todo/controllers/RegisterController.dart';
+import 'package:todo/screens/LoginScreen.dart';
 
 class RegisterScreen extends StatelessWidget {
   RegisterScreen({Key? key}) : super(key: key);
-
+  RegisterController registerController = Get.put(RegisterController());
   String? _newUser;
   String? _newPassword;
   String? _name;
@@ -13,158 +15,177 @@ class RegisterScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: false,
-      body: Form(
-        key: _formKey,
-        child: Padding(
-          padding: const EdgeInsets.only(left: 20.0, right: 20.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(height: 150),
-              TextFormField(
-                autovalidateMode: AutovalidateMode.onUserInteraction,
-                decoration: InputDecoration(
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.purple),
+      resizeToAvoidBottomInset: true,
+      body: SingleChildScrollView(
+        child: Form(
+          key: _formKey,
+          child: Padding(
+            padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(height: 150),
+                TextFormField(
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  decoration: InputDecoration(
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.purple),
+                    ),
+                    labelText: "Mail",
+                    labelStyle: TextStyle(color: Colors.purple),
+                    border: OutlineInputBorder(),
                   ),
-                  labelText: "Mail",
-                  labelStyle: TextStyle(color: Colors.purple),
-                  border: OutlineInputBorder(),
-                ),
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return "Mailinizi Giriniz";
-                  } else {
-                    return null;
-                  }
-                },
-                onSaved: (value) {
-                  _newUser = value!;
-                },
-              ),
-              SizedBox(height: 20.0),
-              TextFormField(
-                autovalidateMode: AutovalidateMode.onUserInteraction,
-                decoration: InputDecoration(
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.purple),
-                  ),
-                  labelText: "Şifre",
-                  labelStyle: TextStyle(color: Colors.purple),
-                  border: OutlineInputBorder(),
-                ),
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return "Şifrenizi Giriniz";
-                  } else {
-                    return null;
-                  }
-                },
-                onSaved: (value) {
-                  _newPassword = value!;
-                },
-              ),
-              SizedBox(height: 20.0),
-              TextFormField(
-                autovalidateMode: AutovalidateMode.onUserInteraction,
-                decoration: InputDecoration(
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.purple),
-                  ),
-                  labelText: "İsim",
-                  labelStyle: TextStyle(color: Colors.purple),
-                  border: OutlineInputBorder(),
-                ),
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return "İsminizi Giriniz";
-                  } else {
-                    return null;
-                  }
-                },
-                onSaved: (value) {
-                  _name = value!;
-                },
-              ),
-              SizedBox(height: 20.0),
-              TextFormField(
-                autovalidateMode: AutovalidateMode.onUserInteraction,
-                decoration: InputDecoration(
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.purple),
-                  ),
-                  labelText: "Soyisim",
-                  labelStyle: TextStyle(color: Colors.purple),
-                  border: OutlineInputBorder(),
-                ),
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return "Soyisminizi Giriniz";
-                  } else {
-                    return null;
-                  }
-                },
-                onSaved: (value) {
-                  _surname = value!;
-                },
-              ),
-              SizedBox(height: 20.0),
-              TextFormField(
-                autovalidateMode: AutovalidateMode.onUserInteraction,
-                decoration: InputDecoration(
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.purple),
-                  ),
-                  labelText: "Yaş",
-                  labelStyle: TextStyle(color: Colors.purple),
-                  border: OutlineInputBorder(),
-                ),
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return "Yaşınızı Giriniz";
-                  } else {
-                    return null;
-                  }
-                },
-                onSaved: (value) {
-                  _age = value!;
-                },
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [],
-              ),
-              ElevatedButton(
-                  child: Text("Kayıt Ol"),
-                  onPressed: () {
-                    if (_formKey.currentState!.validate()) {
-                      _formKey.currentState!.save();
-                    }
-                    if (_newUser!.length > 3 && _newPassword!.length > 3) {
-                      debugPrint("Kayıt başarılı");
-                      //loginController.Login(_username, _password);
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return "Mailinizi Giriniz";
                     } else {
-                      showDialog(
-                          barrierDismissible: false,
-                          context: context,
-                          builder: (BuildContext context) {
-                            return AlertDialog(
-                              title: Text("Hata"),
-                              content: Text("Giriş Bilgileriniz Hatalı"),
-                              actions: [
-                                MaterialButton(
-                                    child: Text("Geri Dön"),
-                                    onPressed: () => Navigator.pop(context))
-                              ],
-                            );
-                          });
+                      return null;
                     }
-                  }),
-              SizedBox(
-                height: 50,
-              ),
-            ],
+                  },
+                  onSaved: (value) {
+                    _newUser = value!;
+                  },
+                ),
+                SizedBox(height: 20.0),
+                TextFormField(
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  decoration: InputDecoration(
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.purple),
+                    ),
+                    labelText: "Şifre",
+                    labelStyle: TextStyle(color: Colors.purple),
+                    border: OutlineInputBorder(),
+                  ),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return "Şifrenizi Giriniz";
+                    } else {
+                      return null;
+                    }
+                  },
+                  onSaved: (value) {
+                    _newPassword = value!;
+                  },
+                ),
+                SizedBox(height: 20.0),
+                TextFormField(
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  decoration: InputDecoration(
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.purple),
+                    ),
+                    labelText: "İsim",
+                    labelStyle: TextStyle(color: Colors.purple),
+                    border: OutlineInputBorder(),
+                  ),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return "İsminizi Giriniz";
+                    } else {
+                      return null;
+                    }
+                  },
+                  onSaved: (value) {
+                    _name = value!;
+                  },
+                ),
+                SizedBox(height: 20.0),
+                TextFormField(
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  decoration: InputDecoration(
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.purple),
+                    ),
+                    labelText: "Soyisim",
+                    labelStyle: TextStyle(color: Colors.purple),
+                    border: OutlineInputBorder(),
+                  ),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return "Soyisminizi Giriniz";
+                    } else {
+                      return null;
+                    }
+                  },
+                  onSaved: (value) {
+                    _surname = value!;
+                  },
+                ),
+                SizedBox(height: 20.0),
+                TextFormField(
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  decoration: InputDecoration(
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.purple),
+                    ),
+                    labelText: "Yaş",
+                    labelStyle: TextStyle(color: Colors.purple),
+                    border: OutlineInputBorder(),
+                  ),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return "Yaşınızı Giriniz";
+                    } else {
+                      return null;
+                    }
+                  },
+                  onSaved: (value) {
+                    _age = value!;
+                  },
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [],
+                ),
+                ElevatedButton(
+                    child: Text("Kayıt Ol"),
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        _formKey.currentState!.save();
+                      }
+                      if (_newUser!.length > 3 && _newPassword!.length > 3) {
+                        debugPrint("Kayıt başarılı");
+                        registerController.Register(_newUser, _newPassword);
+                        showDialog(
+                            barrierDismissible: false,
+                            context: context,
+                            builder: (BuildContext context) {
+                              return AlertDialog(
+                                content: Text("Başarıyla kayıt oldunuz"),
+                                actions: [
+                                  MaterialButton(
+                                      child: Text("Anasayfaya dön"),
+                                      onPressed: () {
+                                        Get.to(LoginScreen());
+                                      })
+                                ],
+                              );
+                            });
+                      } else {
+                        showDialog(
+                            barrierDismissible: false,
+                            context: context,
+                            builder: (BuildContext context) {
+                              return AlertDialog(
+                                title: Text("Hata"),
+                                content: Text("Giriş Bilgileriniz Hatalı"),
+                                actions: [
+                                  MaterialButton(
+                                      child: Text("Geri Dön"),
+                                      onPressed: () {
+                                        Get.to(RegisterScreen());
+                                      })
+                                ],
+                              );
+                            });
+                      }
+                    }),
+                SizedBox(
+                  height: 50,
+                ),
+              ],
+            ),
           ),
         ),
       ),
